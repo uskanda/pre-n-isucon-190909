48da90402176e4f51c9f7a7575df0400f853c0c8
bowlchan <nobody@example.com>
2019-09-09 16:35:44 +0900

    add systemd setting

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cp ./config/nginx.conf /etc/nginx/nginx.conf
 cp ./config/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+cp ./config/isubata.service /etc/systemd/system
+systemctl daemon-reload
 systemctl restart mysql
 systemctl reload nginx
+systemctl start isubata
+systemctl enable isubata
